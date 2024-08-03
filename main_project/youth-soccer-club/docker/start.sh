214c7b7ab915554e63c19cfbda1e42815d6925e7
Neil Fisher <neil3524@gmail.com>
2024-08-03 02:41:12 -0400

    Adjust seeding timing

--- main_project/youth-soccer-club/docker/start.sh
+++ main_project/youth-soccer-club/docker/start.sh
@@ -1,9 +1,15 @@
 docker-entrypoint.sh mysqld &
 
+# wait for mysql to start
 while ! mysqladmin ping -h "$DB_HOST" --silent; do
   sleep 1
 done
 
-cd standalone
+# wait for mysql to fully start
+sleep 30
 
+# wait for server to start, then load the index page to seed the database (hack)
+(sleep 5 && curl -s http://localhost:3000 > /dev/null) &
+
+cd standalone
 HOSTNAME=0.0.0.0 SEED=true node ./server.js
--- main_project/youth-soccer-club/src/app/_util/db.ts
+++ main_project/youth-soccer-club/src/app/_util/db.ts
@@ -2,11 +2,12 @@ import sqlConfig from "@/../mysql.json";
 import mysql2 from "mysql2/promise";
 
 let connection: mysql2.Connection | null = null;
+let connectionPromise: Promise<mysql2.Connection | null> | null = null;
 
 export async function getDb() {
-  let maxTries = 3;
+  let maxTries = 5;
   while (!connection) {
-    connection = await mysql2
+    connectionPromise ??= mysql2
       .createConnection({
         ...sqlConfig.connection,
         multipleStatements: true,
@@ -18,7 +19,12 @@ export async function getDb() {
       .catch((err) => {
         console.error(err);
         return null;
+      })
+      .finally(() => {
+        connectionPromise = null;
       });
+
+    connection = await connectionPromise;
 
     if (maxTries === 0) {
       throw new Error("Could not connect to database");
@@ -26,7 +32,7 @@ export async function getDb() {
     maxTries--;
 
     if (!connection) {
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     }
   }
   return connection;
