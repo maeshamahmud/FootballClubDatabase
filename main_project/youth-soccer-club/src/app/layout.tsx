d9727013a184e4d26d1b84c60251fbf215debba0
Neil Fisher <neil3524@gmail.com>
2024-08-03 05:27:28 -0400

    Keep scrollbar showing to prevent layout shift when switching queries

--- main_project/youth-soccer-club/src/app/layout.tsx
+++ main_project/youth-soccer-club/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-y-scroll">
       <head>
         <meta name="darkreader-lock"></meta>
       </head>
